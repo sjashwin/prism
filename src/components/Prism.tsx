e69ad52e0c8239860efb29f7bea759339ac65ca1
Ashwin Thinnappan <ashwin@codseg.com>
2023-09-25 16:27:42 +0530

    Added Auth Form

---
+++ src/components/Prism.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Passkey } from "../lib/passkey/register";
+
+export interface IButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+    backgroundColor?: string;
+    color?: string;
+};
+
+export const Prism: React.FunctionComponent<IButtonProps> = props => {
+    const { children, backgroundColor, color, style } = props;
+
+    let _style: React.CSSProperties = style || {};
+
+    if (backgroundColor) {
+        _style.backgroundColor = backgroundColor;
+    }
+
+    if (color) {
+        _style.color = color;
+    }
+
+    function submit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const form: FormData = new FormData(e.target as HTMLFormElement);
+        if (form.get("name") as string == null) {
+            return;
+        }
+        Passkey(form.get("name") as string, "name");
+    }
+
+    return (
+        <form onSubmit={submit} className="button">
+            <input type="text" id="name" name="name" placeholder="Enter Name" className="name" />
+            <button
+            style={style} 
+            {...props}
+            type="submit"
+            className="button"
+            >
+
+                {children}  
+            </button>
+        </form>
+    );
+}
---
+++ src/lib/passkey/register.ts
@@ -0,0 +1,46 @@
+const RPID: string = "aslo38o9okdjw94i";
+
+const publicKeyCredentialCreationOptions: PublicKeyCredentialCreationOptions = {
+    challenge: Uint8Array.from(
+        "asjwiendoaskd", c => c.charCodeAt(0)),
+    rp: {
+        name: "Prism Sec",
+    },
+    user: {
+        id: Uint8Array.from(
+            "UZSL85T9AFC", c => c.charCodeAt(0)),
+        name: "",
+        displayName: ""
+    },
+    pubKeyCredParams: [{alg: -7, type: "public-key"}],
+    authenticatorSelection: {
+        authenticatorAttachment: "cross-platform",
+    },
+    timeout: 60000,
+    attestation: "direct" as const
+};
+
+export const Passkey = async (name: string, displayName: string, rpId?: string, challenge?: string) => {
+    if(name.length < 1 || displayName.length < 1) {
+        console.log("Name or Display Name is empty");
+        return null;
+    }
+    publicKeyCredentialCreationOptions.rp.id = window.location.hostname || rpId;
+    publicKeyCredentialCreationOptions.user.id = Uint8Array.from(
+        (challenge || RPID), c => c.charCodeAt(0)
+        );
+    publicKeyCredentialCreationOptions.user.name = ""
+    let credential: Credential | null = null;
+    publicKeyCredentialCreationOptions.user.displayName = displayName;
+    publicKeyCredentialCreationOptions.user.name = name;
+
+    try {
+        credential = await navigator.credentials.create({
+            publicKey: publicKeyCredentialCreationOptions
+        });
+    } catch (error) {
+        console.error(error);
+    } finally {
+        return credential;
+    }
+};
